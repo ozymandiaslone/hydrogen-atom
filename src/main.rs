5f518176fab67804527147598b6e033a5b4930e2
ozymandiaslone <saltsperkins@gmail.com>
2025-01-21 21:05:43 +0000

    basically nothing

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ use ui::*;
 #[macroquad::main("Hydrogen Atom :)")]
 async fn main() {
   let mut mosiac = Mosaic {
+    // TODO we are gonna want to instead send a query
+    // to the server to get the probability amplitudes
+    // and then perhaps on the client side (here) we use 
+    // that data to construct images -> Texture2D
     eigenstates: calculate_n_states(6),
     position: (0., 0.),
     zoom: 0.
